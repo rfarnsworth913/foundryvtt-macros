8e100f0ae5dcd12a5233222a874891808e7d52a0
Richard Farnsworth <rfarnsworth913@gmail.com>
2021-12-25 16:30:18 -0500

    Fixed mispelling

--- Trigger Happy/Detect.js
+++ Trigger Happy/Detect.js
@@ -47,7 +47,7 @@
         // Handle Traps -------------------------------------------------------
         if (props.type === "Trap") {
             if (canvas.tokens.get(props.targetID).data.hidden) {
-                canvas.tokens.get(props.targetId).update({ hidden: false });
+                canvas.tokens.get(props.targetID).update({ hidden: false });
 
                 let message = `You have spotted a trap!`;
                 let chatData = {
