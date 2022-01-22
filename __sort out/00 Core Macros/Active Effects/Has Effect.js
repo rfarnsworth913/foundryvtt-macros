3a7301afb099ccf727b96dc2f2c72efb3a6217cd
Richard Farnsworth <rfarnsworth913@gmail.com>
2022-01-22 17:08:04 -0500

    Has Effect moved into code snippet

--- __sort out/00 Core Macros/Active Effects/Has Effect.js
+++
@@ -1,4 +0,0 @@
-function hasEffect({ actor, effect = ``} = {}){
-  if(!actor) return console.error(`Actor Error`);
-  return Boolean(actor.effects.find(effect => effect.data.label.toLowerCase() === effect.toLowerCase()));
-}
