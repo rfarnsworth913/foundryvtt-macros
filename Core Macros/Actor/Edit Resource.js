e115bce471218ce7422ec983ff4bdcc2899fadf9
Richard Farnsworth <rfarnsworth913@gmail.com>
2022-02-02 19:55:00 -0500

    Update folder structure and Edit Resource macro

---
+++ Core Macros/Actor/Edit Resource.js
@@ -0,0 +1,37 @@
+/**
+ * Handles updating a global resource on the specified actor
+ *
+ * @param    {object} [options]
+ * @param    {Actor5e}  actor  Target actor to update
+ * @param    {string}   name   Name of resource to be updated
+ * @param    {number}   value  New value of the resource
+ * @returns  {Promise<any>}    Actor update handler
+ */
+async function editResource ({ actor, name = "", value = 1} = {}) {
+
+    // Check actor and name
+    if (!actor || !name) {
+        return {};
+    }
+
+    // Attempt to find object on the specified actor
+    let resources = actor.toObject().data.resources;
+    let [key, object] = Object.entries(resources).find(([key, object]) => {
+        return key === name || object.label === name;
+    });
+
+    if (!key || !object) {
+        return {};
+    }
+
+    // Attempt to update the resource with the specified value
+    if (!object.value || !object.max) {
+        object.value = object.max = value;
+    } else {
+        object.value = Math.clamped(object.value + value, 0, object.max);
+    }
+
+    resources[key] = object;
+
+    return await actor.update({ "data.resources": resources });
+}
