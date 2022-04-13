27472f1d4b8274a1313117865e1acf56530033f1
Richard Farnsworth <rfarnsworth913@gmail.com>
2022-04-12 20:50:17 -0400

    Update Has Effect

--- Core Macros/Active Effects/Has Effect.js
+++ Core Macros/Active Effects/Has Effect.js
@@ -2,16 +2,16 @@
  * Checks if a specified actor has the expected effect applied to their character
  *
  * @param    {object}  [options]
- * @param    {Actor5e}  actor         Target Actor
+ * @param    {Actor5e}  actorData     Target Actor
  * @param    {string}   effectLabel   Effect to be found on target actor
  * @returns  {Promise<Boolean>}       Status of the effect on target
  */
-async function hasEffect({ actor, effectLabel = `` } = {}) {
-    if (!actor) {
+async function hasEffect({ actorData, effectLabel = `` } = {}) {
+    if (!actorData) {
         return console.error("No actor specified!");
     }
 
-    return Boolean(actor.effects.find((effect) => {
+    return Boolean(actorData.effects.find((effect) => {
         return effect.data.label.toLowerCase() === effectLabel.toLowerCase()
     }));
 }
