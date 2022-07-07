991b9e8f785fbc7a9947809f7c79ae22e89e8dfc
Richard Farnsworth <rfarnsworth913@gmail.com>
2022-07-07 16:55:21 -0400

    Updated Faerie Fire

--- dnd5e/spells/1st level/faerie-fire.js
+++ dnd5e/spells/1st level/faerie-fire.js
@@ -11,6 +11,7 @@ const props = {
     name: "Faerie Fire",
     state: args[0]?.macroPass || args[0] || "unknown",
 
+    color: "red",
     templateID: args[0]?.templateId || ""
 };
 
@@ -20,10 +21,55 @@ logProps(props);
 /* ==========================================================================
     Macro Logic
    ========================================================================== */
-if (props.state === "postActiveEffects") {
-    await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [props.templateID]);
-    game.user.updateTokenTargets([]);
-    game.user.broadcastActivity({ targets: [] });
+if (props.state === "preActiveEffects") {
+
+    // Animate applying effect ------------------------------------------------
+    if (game.modules.get("sequencer")?.active) {
+        const template = canvas.templates.placeables.find((template) => {
+            return template.id === props.templateID;
+        });
+
+        console.warn(template);
+
+        if (!template) {
+            return false;
+        }
+
+        new Sequence()
+            .effect()
+                .file(`jb2a.fireflies.few.01.${props.color}`)
+                .atLocation(template)
+                .scaleToObject()
+                .fadeOut(500)
+            .effect()
+                .file(`jb2a.fireflies.few.02.${props.color}`)
+                .atLocation(template)
+                .scaleToObject()
+                .fadeOut(500)
+            .effect()
+                .file(`jb2a.fireflies.many.01.${props.color}`)
+                .atLocation(template)
+                .scaleToObject()
+                .fadeOut(500)
+            .effect()
+                .file(`jb2a.fireflies.many.02.${props.color}`)
+                .atLocation(template)
+                .scaleToObject()
+                .fadeOut(500)
+            .waitUntilFinished()
+            .thenDo(async () => {
+                await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [props.templateID]);
+                game.user.updateTokenTargets([]);
+                game.user.broadcastActivity({ targets: [] });
+            })
+            .play();
+
+    // Handle normal removal of measured template -----------------------------
+    } else {
+        await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [props.templateID]);
+        game.user.updateTokenTargets([]);
+        game.user.broadcastActivity({ targets: [] });
+    }
 }
 
 
