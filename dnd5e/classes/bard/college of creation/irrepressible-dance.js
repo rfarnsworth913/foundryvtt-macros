a34b9eb98b238549af117739f2d88edeeecbcfe9
Richard Farnsworth <rfarnsworth913@gmail.com>
2022-07-25 19:18:40 -0400

    Starting point

---
+++ dnd5e/classes/bard/college of creation/irrepressible-dance.js
@@ -0,0 +1,49 @@
+/* ==========================================================================
+    Macro:         Irrepressible Dance
+    Source:        Custom
+    Usage:         DAE ItemMacro
+   ========================================================================== */
+
+/* ==========================================================================
+    Macro Globals
+   ========================================================================== */
+const lastArg   = args[args.length - 1];
+const tokenData = canvas.tokens.get(lastArg?.tokenId) || {};
+
+const props = {
+    name: "Irrepressible Dance",
+    state: args[0]?.tag || args[0] || "unknown",
+
+    actorData: tokenData?.actor || {},
+    tokenData,
+
+    lastArg,
+
+
+};
+
+logProps(props);
+
+
+/* ==========================================================================
+    Macro Logic
+   ========================================================================== */
+
+
+
+/* ==========================================================================
+    Helpers
+   ========================================================================== */
+
+/**
+* Logs the global properties for the Macro to the console for debugging purposes
+*
+* @param  {Object}  props  Global properties
+*/
+function logProps (props) {
+    console.group(`${props.name} Macro`);
+    Object.keys(props).forEach((key) => {
+        console.log(`${key}: `, props[key]);
+    });
+    console.groupEnd();
+}
